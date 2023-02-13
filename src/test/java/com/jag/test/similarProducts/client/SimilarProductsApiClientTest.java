77cf884279dc01bbb7f3e5af0c4bfbee07e74ad3
JOSE ANTONIO GARCIA PEREZ <jagarcia.perez@atsistemas.com>
2023-02-13 09:04:32 +0100

    Mod tests SimilarProductsApiClientTest

--- src/test/java/com/jag/test/similarProducts/client/SimilarProductsApiClientTest.java
+++ src/test/java/com/jag/test/similarProducts/client/SimilarProductsApiClientTest.java
@@ -1,6 +1,7 @@
 package com.jag.test.similarProducts.client;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.openapitools.model.ProductDetail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,15 +10,25 @@ import java.math.BigDecimal;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest()
 class SimilarProductsApiClientTest {
 
-    @Autowired
+    @Mock
     private SimilarProductsApiClient similarProductsApiClient;
 
     @Test
     void getProductId() {
+        ProductDetail productDetail = new ProductDetail()
+                .id("1")
+                .availability(Boolean.TRUE)
+                .name("Shirt")
+                .price(BigDecimal.valueOf(9.99));
+
+        when(similarProductsApiClient.getProductId(anyString())).thenReturn(productDetail);
+
         ProductDetail response = similarProductsApiClient.getProductId("1");
 
         assertNotNull(response);
@@ -29,10 +40,12 @@ class SimilarProductsApiClientTest {
 
     @Test
     void getProductSimilarIds() {
+        Set<String> result = Set.of("1", "2", "3");
+        when(similarProductsApiClient.getProductSimilarIds(anyString())).thenReturn(result);
+
         Set<String> response = similarProductsApiClient.getProductSimilarIds("1");
 
         assertNotNull(response);
-        Set<String> result = Set.of("2", "3", "4");
         assertEquals(response, result);
     }
 }
